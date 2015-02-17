b8c65f4876da686bcd0f2b50385ddbca9a2c2135
Łukasz Kędziorski <lk346959@students.mimuw.edu.pl>
2015-02-17 21:44:18 +0100

    Poprawki co do dostepu do bazy danych wordpress'a i sanityzacja formy

--- bdlab.php
+++ bdlab.php
@@ -1,47 +1,92 @@
 <?php
+
 /*
 Plugin Name: Strona Domowa
 Author: lk346959
  */
 
+function rmlc_install()
+{
+    global $wpdb;
+    $prefix = $wpdb->prefix;
+    $tablename = $prefix . "employment";
+    $sql = "CREATE TABLE $tablename (
+            userid int(11) NOT NULL,
+            company varchar(255) NOT NULL,
+            time int(11) NOT NULL,
+            role varchar(255) NOT NULL,
+        )";
+    dbDelta($sql);
+
+    $tablename = $prefix . "portfolio";
+    $sql = "CREATE TABLE $tablename (
+            userid int(11) NOT NULL,
+            description varchar(255) NOT NULL,
+            role varchar(255) NOT NULL,
+            link varchar(255) NOT NULL,
+        )";
+    dbDelta($sql);
+
+    $tablename = $prefix . "prizes";
+    $sql = "CREATE TABLE $tablename (
+            userid int(11) NOT NULL,
+            name varchar(255) NOT NULL,
+            date datetime NOT NULL,
+            info varchar(255) NOT NULL,
+            link varchar(255) NOT NULL,
+        )";
+    dbDelta($sql);
+
+    $tablename = $prefix . "technology";
+    $sql = "CREATE TABLE $tablename (
+            userid int(11) NOT NULL,
+            name varchar(255) NOT NULL,
+            experience varchar(255) NOT NULL,
+            link varchar(255) NOT NULL,
+        )";
+    dbDelta($sql);
+}
+
+function rmlc_uninstall()
+{
+    global $wpdb;
+    $prefix = $wpdb->prefix;
+    $tablename = $prefix . "employment";
+    $wpdb->query("DROP TABLE IF EXISTS " . $tablename);
+    $tablename = $prefix . "portfolio";
+    $wpdb->query("DROP TABLE IF EXISTS " . $tablename);
+    $tablename = $prefix . "prizes";
+    $wpdb->query("DROP TABLE IF EXISTS " . $tablename);
+    $tablename = $prefix . "technology";
+    $wpdb->query("DROP TABLE IF EXISTS " . $tablename);
+}
+
 // 1.
 add_shortcode('plan', 'timetable');
 function timetable()
 {
-    $servername = "labdb.mimuw.edu.pl";
-    $username = "lk346959";
-    $password = "ClujeeHinMyffeb\"";
-    $dbname = "lk346959";
-
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-    if (!$conn)
-    {
-        die("Connection failed: " . mysqli_connect_error() . "</br>");
-    }
-    //echo "Connected successfully</br>";
     global $current_user;
     global $wpdb;
     $sql = "SELECT * FROM ".($wpdb->prefix)."timetable WHERE userid=";
     $sql .= "(SELECT identifier FROM ".($wpdb->prefix)."wslusersprofiles WHERE user_id=".$current_user->id.")";
  
-    $result = mysqli_query($conn, $sql);
+    $result = $wpdb->get_results($sql);
 
     echo "Plan na tydzien:</br>";
-    if (mysqli_num_rows($result) > 0)
+    if ($result)
     {
         echo "<table>";
-        while ($row = mysqli_fetch_assoc($result))
+        foreach ($result as $row)
         {
             echo "<tr>";
             echo "<td>";
-            echo $row["name"];
+            echo $row->name;
             echo "</td>";
             echo "<td>";
-            echo $row["start_time"];
+            echo $row->start_time;
             echo "</td>";
             echo "<td>";
-            echo $row["end_time"];
+            echo $row->end_time;
             echo "</td>";
             echo "</tr>";
         }
@@ -51,40 +96,27 @@ function timetable()
     {
         echo "Brak planu</br>";
     }
-    mysqli_close($conn);
 }
 
 // 2.
 add_shortcode('zaliczone', 'passed');
 function passed()
 {
-    $servername = "labdb.mimuw.edu.pl";
-    $username = "lk346959";
-    $password = "ClujeeHinMyffeb\"";
-    $dbname = "lk346959";
-
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-    if (!$conn)
-    {
-        die("Connection failed: " . mysqli_connect_error() . "</br>");
-    }
-    //echo "Connected successfully</br>";
     global $current_user;
     global $wpdb;
     $sql = "SELECT * FROM ".($wpdb->prefix)."courses WHERE userid=";
     $sql .= "(SELECT identifier FROM ".($wpdb->prefix)."wslusersprofiles WHERE user_id=".$current_user->id.")";
  
-    $result = mysqli_query($conn, $sql);
+    $result = $wpdb->get_results($sql);
 
     echo "Togo juz sie nauczyl:</br>";
-    if (mysqli_num_rows($result) > 0)
+    if ($result)
     {
         echo "<ul>";
-        while ($row = mysqli_fetch_assoc($result))
+        foreach ($result as $row)
         {
             echo "<li>";
-            echo $row["name"];
+            echo $row->name;
             echo "</li>";
         }
         echo "</ul>";
@@ -93,40 +125,27 @@ function passed()
     {
         echo "Brak przedmiotow</br>";
     }
-    mysqli_close($conn);
 }
 
 // 3.
 add_shortcode('uczyteraz', 'nowlearning');
 function nowlearning()
 {
-    $servername = "labdb.mimuw.edu.pl";
-    $username = "lk346959";
-    $password = "ClujeeHinMyffeb\"";
-    $dbname = "lk346959";
-
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-    if (!$conn)
-    {
-        die("Connection failed: " . mysqli_connect_error() . "</br>");
-    }
-    //echo "Connected successfully</br>";
     global $current_user;
     global $wpdb;
     $sql = "SELECT * FROM ".($wpdb->prefix)."currentcourses WHERE userid=";
     $sql .= "(SELECT identifier FROM ".($wpdb->prefix)."wslusersprofiles WHERE user_id=".$current_user->id.")";
  
-    $result = mysqli_query($conn, $sql);
+    $result = $wpdb->get_results($sql);
 
     echo "Teraz uczy sie:</br>";
-    if (mysqli_num_rows($result) > 0)
+    if ($result)
     {
         echo "<ul>";
-        while ($row = mysqli_fetch_assoc($result))
+        foreach ($result as $row)
         {
             echo "<li>";
-            echo $row["name"];
+            echo $row->name;
             echo "</li>";
         }
         echo "</table>";
@@ -135,40 +154,27 @@ function nowlearning()
     {
         echo "Brak przedmiotow</br>";
     }
-    mysqli_close($conn);
 }
 
 // 4.
 add_shortcode('zatrudnienie', 'employment');
 function employment()
 {
-    $servername = "labdb.mimuw.edu.pl";
-    $username = "lk346959";
-    $password = "ClujeeHinMyffeb\"";
-    $dbname = "lk346959";
-
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-    if (!$conn)
-    {
-        die("Connection failed: " . mysqli_connect_error() . "</br>");
-    }
-    //echo "Connected successfully</br>";
     global $current_user;
     global $wpdb;
-    $sql = "SELECT * FROM employment WHERE userid=";
+    $sql = "SELECT * FROM ".($wpdb->prefix)."employment WHERE userid=";
     $sql .= "(SELECT identifier FROM ".($wpdb->prefix)."wslusersprofiles WHERE user_id=".$current_user->id.")";
  
-    $result = mysqli_query($conn, $sql);
+    $result = $wpdb->get_results($sql);
 
     echo "Zatrudnienie:</br>";
-    if (mysqli_num_rows($result) > 0)
+    if ($result)
     {
         echo "<ul>";
-        while ($row = mysqli_fetch_assoc($result))
+        foreach ($result as $row)
         {
             echo "<li>";
-            echo $row["company"]." - ".$row["time"]." mies. - ".$row["role"];
+            echo $row->company." - ".$row->time." mies. - ".$row->role;
             echo "</li>";
         }
         echo "</ul>";
@@ -177,40 +183,27 @@ function employment()
     {
         echo "Brak zatrudnien</br>";
     }
-    mysqli_close($conn);
 }
 
 // 5.
 add_shortcode('portfolio', 'portfolio');
 function portfolio()
 {
-    $servername = "labdb.mimuw.edu.pl";
-    $username = "lk346959";
-    $password = "ClujeeHinMyffeb\"";
-    $dbname = "lk346959";
-
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-    if (!$conn)
-    {
-        die("Connection failed: " . mysqli_connect_error() . "</br>");
-    }
-    //echo "Connected successfully</br>";
     global $current_user;
     global $wpdb;
-    $sql = "SELECT * FROM portfolio WHERE userid=";
+    $sql = "SELECT * FROM ".($wpdb->prefix)."portfolio WHERE userid=";
     $sql .= "(SELECT identifier FROM ".($wpdb->prefix)."wslusersprofiles WHERE user_id=".$current_user->id.")";
  
-    $result = mysqli_query($conn, $sql);
+    $result = $wpdb->get_results($sql);
 
     echo "Portfolio:</br>";
-    if (mysqli_num_rows($result) > 0)
+    if ($result)
     {
         echo "<ul>";
-        while ($row = mysqli_fetch_assoc($result))
+        foreach ($result as $row)
         {
             echo "<li>";
-            echo $row["description"]." - ".$row["role"]." - <a href=\"".$row["link"]."\">Probka</a>";
+            echo $row->description." - ".$row->role." - <a href=\"".$row->link."\">Probka</a>";
             echo "</li>";
         }
         echo "</ul>";
@@ -219,41 +212,57 @@ function portfolio()
     {
         echo "Brak projektow w portfolio</br>";
     }
-    mysqli_close($conn);
 }
 
 // 6.
 add_shortcode('technologie', 'technologies');
 function technologies()
 {
-    $servername = "labdb.mimuw.edu.pl";
-    $username = "lk346959";
-    $password = "ClujeeHinMyffeb\"";
-    $dbname = "lk346959";
-
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-    if (!$conn)
-    {
-        die("Connection failed: " . mysqli_connect_error() . "</br>");
-    }
-    //echo "Connected successfully</br>";
     global $current_user;
     global $wpdb;
-    $sql = "SELECT * FROM technologies WHERE userid=";
+    $dzialaj = 1;
+    if (isset($_POST["prizename"]))
+    {
+        $prizeuserid = $_POST["prizeuserid"];
+        $prizename = filter_var($_POST["prizename"], FILTER_SANITIZE_STRING);
+        if (!$prizename)
+        {
+            echo "Brak nazwy!</br>";
+            $dzialaj = 0;
+        }
+        $prizeexperience = filter_var($_POST["prizeexperience"], FILTER_SANITIZE_STRING);
+        if (!$prizeexperience)
+        {
+            echo "Brak doswiadczenia!</br>";
+            $dzialaj = 0;
+        }
+        $prizelink = filter_var($_POST["prizelink"], FILTER_SANITIZE_URL);
+        if (!filter_var($prizelink, FILTER_VALIDATE_URL))
+        {
+            echo "Zly link!</br>";
+            $dzialaj = 0;
+        }
+        if ($dzialaj == 1)
+        {
+            $sql='INSERT INTO lk346959.technologies(userid, name, experience, link) VALUES (\''.$prizeuserid.'\',\''.$prizename.'\', \''.$prizeexperience.'\', \''.$prizelink.'\')';
+            $wpdb->query($sql);
+            echo '<h1>Dodany nowy wpis!</h1></br></br>';
+        }
+    }
+    $sql = "SELECT * FROM ".($wpdb->prefix)."technologies WHERE userid=";
     $numerek .= "(SELECT identifier FROM ".($wpdb->prefix)."wslusersprofiles WHERE user_id=".$current_user->id.")";
     $sql .= $numerek;
  
-    $result = mysqli_query($conn, $sql);
+    $result = $wpdb->get_results($sql);
 
     echo "Technologie:</br>";
-    if (mysqli_num_rows($result) > 0)
+    if ($result)
     {
         echo "<ul>";
-        while ($row = mysqli_fetch_assoc($result))
+        foreach ($result as $row)
         {
             echo "<li>";
-            echo $row["name"]." - ".$row["experience"]." - <a href=\"".$row["link"]."\">Probka</a>";
+            echo $row->name." - ".$row->experience." - <a href=\"".$row->link."\">Probka</a>";
             echo "</li>";
         }
         echo "</ul>";
@@ -263,17 +272,14 @@ function technologies()
         echo "Brak technologii</br>";
     }
     
-    $res2 = mysqli_query($conn, $numerek);
-    $num = mysqli_fetch_assoc($res2);
-
-    mysqli_close($conn);
-
+    $res2 = $wpdb->get_col($numerek);
+ 
     echo 'Dodaj technologie:</br>
-        <form method="get" action="wp-content/plugins/addtech.php">
-        Nazwa: <input type="text" name="name"></br>
-        Doswiadczenie: <input type="text" name="experience"></br>
-        Probka: <input type="text" name="link"></br>
-        <input type="hidden" name="userid" value="'.$num["identifier"].'">
+        <form method="post" action="">
+        Nazwa: <input type="text" name="prizename"></br>
+        Doswiadczenie: <input type="text" name="prizeexperience"></br>
+        Probka: <input type="text" name="prizelink"></br>
+        <input type="hidden" name="prizeuserid" value="'.$res2[0].'">
         <input type="submit" name="button">
         </form>
         ';
@@ -283,32 +289,20 @@ function technologies()
 add_shortcode('nagrody', 'prizes');
 function prizes()
 {
-    $servername = "labdb.mimuw.edu.pl";
-    $username = "lk346959";
-    $password = "ClujeeHinMyffeb\"";
-    $dbname = "lk346959";
-
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-    if (!$conn)
-    {
-        die("Connection failed: " . mysqli_connect_error() . "</br>");
-    }
-    //echo "Connected successfully</br>";
     global $current_user;
     global $wpdb;
-    $sql = "SELECT * FROM prizes WHERE userid=";
+    $sql = "SELECT * FROM ".($wpdb->prefix)."prizes WHERE userid=";
     $sql .= "(SELECT identifier FROM ".($wpdb->prefix)."wslusersprofiles WHERE user_id=".$current_user->id.")";
-    $result = mysqli_query($conn, $sql);
+    $result = $wpdb->get_results($sql);
 
     echo "Nagrody:</br>";
-    if (mysqli_num_rows($result) > 0)
+    if ($result)
     {
         echo "<ul>";
-        while ($row = mysqli_fetch_assoc($result))
+        foreach ($result as $row)
         {
             echo "<li>";
-            echo $row["name"]." - ".$row["date"]." - ".$row["info"]." - <a href=\"".$row["link"]."\">Probka</a>";
+            echo $row->name." - ".$row->date." - ".$row->info." - <a href=\"".$row->link."\">Probka</a>";
             echo "</li>";
         }
         echo "</ul>";
@@ -317,8 +311,6 @@ function prizes()
     {
         echo "Brak nagrod</br>";
     }
-    mysqli_close($conn);
 }
 ?>
-
 
